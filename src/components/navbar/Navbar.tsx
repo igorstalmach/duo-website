79396956d3d3e419249d4750a08f582e4dcfaf45
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-04-08 15:35:35 +0200

    add button

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,18 +5,9 @@ import { IconMenuClosed } from "@/assets/icons/IconMenuClosed";
 import { IconMenuOpen } from "@/assets/icons/IconMenuOpen";
 import { Navlinks } from "../navlinks/Navlinks";
 import Link from "next/link";
-import { useTranslation } from "next-i18next";
-import { useRouter } from "next/router";
 
 export const Navbar = () => {
-  const router = useRouter();
   const [menuState, setMenuState] = useState<boolean>(false);
-  const onToggleLanguageClick = (newLocale: string) => {
-    const { pathname, asPath, query } = router;
-    router.push({ pathname, query }, asPath, { locale: newLocale });
-  };
-  const changeTo = router.locale === "pl" ? "en" : "pl";
-  const [t] = useTranslation();
 
   return (
     <div className={styles.container}>
@@ -34,9 +25,6 @@ export const Navbar = () => {
           <div className={!menuState ? styles.linksHidden : undefined}>
             <Navlinks toHamburgerMenu={true} />
           </div>
-          <button onClick={() => onToggleLanguageClick(changeTo)}>
-            {t("change-locale", { changeTo })}
-          </button>
         </div>
       </div>
     </div>
--- src/components/navlinks/Navlinks.tsx
+++ src/components/navlinks/Navlinks.tsx
@@ -2,8 +2,18 @@ import React from "react";
 import styles from "./Navlinks.module.scss";
 import Link from "next/link";
 import { INavlinksProps } from "./INavlinksProps";
+import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
 
 export const Navlinks = (props: INavlinksProps) => {
+  const router = useRouter();
+  const onToggleLanguageClick = (newLocale: string) => {
+    const { pathname, asPath, query } = router;
+    router.push({ pathname, query }, asPath, { locale: newLocale });
+  };
+  const changeTo = router.locale === "pl" ? "en" : "pl";
+  const [t] = useTranslation();
+
   return (
     <nav
       className={
@@ -47,6 +57,12 @@ export const Navlinks = (props: INavlinksProps) => {
       >
         KONTAKT
       </Link>
+      <button
+        className={styles.link}
+        onClick={() => onToggleLanguageClick(changeTo)}
+      >
+        {t("changeLocale", { changeTo })}
+      </button>
     </nav>
   );
 };
