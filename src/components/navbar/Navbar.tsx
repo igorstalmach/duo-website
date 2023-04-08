354a8c75eee0c866ebd06fa49f7bc253bc8368cc
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-04-08 02:03:36 +0200

    layout fixes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,37 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.scss";
 import { IconLogo } from "@/assets/icons/IconLogo";
 import { IconMenuClosed } from "@/assets/icons/IconMenuClosed";
 import { IconMenuOpen } from "@/assets/icons/IconMenuOpen";
 import { Navlinks } from "../navlinks/Navlinks";
-import { joinClasses } from "@/common/utils/joinClasses";
 import Link from "next/link";
 
 export const Navbar = () => {
   const [menuState, setMenuState] = useState<boolean>(false);
 
-  const [scrollPosition, setScrollPosition] = useState<number>(0);
-  const handleScroll = () => {
-    const position = window.scrollY;
-    setScrollPosition(position);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll, { passive: true });
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <div
-      className={
-        scrollPosition > 70
-          ? joinClasses(styles.container, styles.smallContainer)
-          : styles.container
-      }
-    >
+    <div className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles.logoContainer}>
           <Link href={"/"}>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import "@/styles/Global.scss";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
