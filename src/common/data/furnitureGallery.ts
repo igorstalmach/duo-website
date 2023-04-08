de7c6f5d45126e1d498893b363ff1f320c9a0e96
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-04-09 00:16:21 +0200

    add furniture gallery

---
+++ src/common/data/furnitureGallery.ts
@@ -0,0 +1,340 @@
+import { IGalleryDataProps } from "../interfaces/IGalleryDataProps";
+import img_1 from "@/assets/images/furniture/image-1.jpeg";
+import img_2 from "@/assets/images/furniture/image-2.jpeg";
+import img_3 from "@/assets/images/furniture/image-3.jpeg";
+import img_4 from "@/assets/images/furniture/image-4.jpeg";
+import img_5 from "@/assets/images/furniture/image-5.jpeg";
+import img_6 from "@/assets/images/furniture/image-6.jpeg";
+import img_7 from "@/assets/images/furniture/image-7.jpeg";
+import img_8 from "@/assets/images/furniture/image-8.jpeg";
+import img_9 from "@/assets/images/furniture/image-9.jpeg";
+import img_10 from "@/assets/images/furniture/image-10.jpeg";
+import img_11 from "@/assets/images/furniture/image-11.jpeg";
+import img_12 from "@/assets/images/furniture/image-12.jpeg";
+import img_13 from "@/assets/images/furniture/image-13.jpeg";
+import img_14 from "@/assets/images/furniture/image-14.jpeg";
+import img_15 from "@/assets/images/furniture/image-15.jpeg";
+import img_16 from "@/assets/images/furniture/image-16.jpeg";
+import img_17 from "@/assets/images/furniture/image-17.jpeg";
+import img_18 from "@/assets/images/furniture/image-18.jpeg";
+import img_19 from "@/assets/images/furniture/image-19.jpeg";
+import img_20 from "@/assets/images/furniture/image-20.jpeg";
+import img_21 from "@/assets/images/furniture/image-21.jpeg";
+import img_22 from "@/assets/images/furniture/image-22.jpeg";
+import img_23 from "@/assets/images/furniture/image-23.jpeg";
+import img_24 from "@/assets/images/furniture/image-24.jpeg";
+import img_25 from "@/assets/images/furniture/image-25.jpeg";
+import img_26 from "@/assets/images/furniture/image-26.jpeg";
+import img_27 from "@/assets/images/furniture/image-27.jpeg";
+import img_28 from "@/assets/images/furniture/image-28.jpeg";
+import img_29 from "@/assets/images/furniture/image-29.jpeg";
+import img_30 from "@/assets/images/furniture/image-30.jpeg";
+import img_31 from "@/assets/images/furniture/image-31.jpeg";
+import img_32 from "@/assets/images/furniture/image-32.jpeg";
+import img_33 from "@/assets/images/furniture/image-33.jpeg";
+import img_34 from "@/assets/images/furniture/image-34.jpeg";
+import img_35 from "@/assets/images/furniture/image-35.jpeg";
+import img_36 from "@/assets/images/furniture/image-36.jpeg";
+import img_37 from "@/assets/images/furniture/image-37.jpeg";
+import img_38 from "@/assets/images/furniture/image-38.jpeg";
+import img_39 from "@/assets/images/furniture/image-39.jpeg";
+import img_40 from "@/assets/images/furniture/image-40.jpeg";
+import img_41 from "@/assets/images/furniture/image-41.jpeg";
+import img_42 from "@/assets/images/furniture/image-42.jpeg";
+import img_43 from "@/assets/images/furniture/image-43.jpeg";
+import img_44 from "@/assets/images/furniture/image-44.jpeg";
+import img_45 from "@/assets/images/furniture/image-45.jpeg";
+import img_46 from "@/assets/images/furniture/image-46.jpeg";
+import img_47 from "@/assets/images/furniture/image-47.jpeg";
+import img_48 from "@/assets/images/furniture/image-48.jpeg";
+
+export const furnitureGallery: IGalleryDataProps[] = [
+  {
+    id: 1,
+    src: img_1,
+    url: "@/assets/images/furniture/image-1.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 2,
+    src: img_2,
+    url: "@/assets/images/furniture/image-2.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 3,
+    src: img_3,
+    url: "@/assets/images/furniture/image-3.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 4,
+    src: img_4,
+    url: "@/assets/images/furniture/image-4.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 5,
+    src: img_5,
+    url: "@/assets/images/furniture/image-5.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 6,
+    src: img_6,
+    url: "@/assets/images/furniture/image-6.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 7,
+    src: img_7,
+    url: "@/assets/images/furniture/image-7.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 8,
+    src: img_8,
+    url: "@/assets/images/furniture/image-8.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 9,
+    src: img_9,
+    url: "@/assets/images/furniture/image-9.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 10,
+    src: img_10,
+    url: "@/assets/images/furniture/image-10.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 11,
+    src: img_11,
+    url: "@/assets/images/furniture/image-11.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 12,
+    src: img_12,
+    url: "@/assets/images/furniture/image-12.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 13,
+    src: img_13,
+    url: "@/assets/images/furniture/image-13.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 14,
+    src: img_14,
+    url: "@/assets/images/furniture/image-14.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 15,
+    src: img_15,
+    url: "@/assets/images/furniture/image-15.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 16,
+    src: img_16,
+    url: "@/assets/images/furniture/image-16.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 17,
+    src: img_17,
+    url: "@/assets/images/furniture/image-17.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 18,
+    src: img_18,
+    url: "@/assets/images/furniture/image-18.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 19,
+    src: img_19,
+    url: "@/assets/images/furniture/image-19.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 20,
+    src: img_20,
+    url: "@/assets/images/furniture/image-20.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 21,
+    src: img_21,
+    url: "@/assets/images/furniture/image-21.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 22,
+    src: img_22,
+    url: "@/assets/images/furniture/image-22.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 23,
+    src: img_23,
+    url: "@/assets/images/furniture/image-23.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 24,
+    src: img_24,
+    url: "@/assets/images/furniture/image-24.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 25,
+    src: img_25,
+    url: "@/assets/images/furniture/image-25.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 26,
+    src: img_26,
+    url: "@/assets/images/furniture/image-26.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 27,
+    src: img_27,
+    url: "@/assets/images/furniture/image-27.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 28,
+    src: img_28,
+    url: "@/assets/images/furniture/image-28.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 29,
+    src: img_29,
+    url: "@/assets/images/furniture/image-29.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 30,
+    src: img_30,
+    url: "@/assets/images/furniture/image-30.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 31,
+    src: img_31,
+    url: "@/assets/images/furniture/image-31.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 32,
+    src: img_32,
+    url: "@/assets/images/furniture/image-32.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 33,
+    src: img_33,
+    url: "@/assets/images/furniture/image-33.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 34,
+    src: img_34,
+    url: "@/assets/images/furniture/image-34.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 35,
+    src: img_35,
+    url: "@/assets/images/furniture/image-35.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 36,
+    src: img_36,
+    url: "@/assets/images/furniture/image-36.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 37,
+    src: img_37,
+    url: "@/assets/images/furniture/image-37.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 38,
+    src: img_38,
+    url: "@/assets/images/furniture/image-38.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 39,
+    src: img_39,
+    url: "@/assets/images/furniture/image-39.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 40,
+    src: img_40,
+    url: "@/assets/images/furniture/image-40.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 41,
+    src: img_41,
+    url: "@/assets/images/furniture/image-41.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 42,
+    src: img_42,
+    url: "@/assets/images/furniture/image-42.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 43,
+    src: img_43,
+    url: "@/assets/images/furniture/image-43.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 44,
+    src: img_44,
+    url: "@/assets/images/furniture/image-44.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 45,
+    src: img_45,
+    url: "@/assets/images/furniture/image-45.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 46,
+    src: img_46,
+    url: "@/assets/images/furniture/image-46.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 47,
+    src: img_47,
+    url: "@/assets/images/furniture/image-47.jpeg",
+    alt: "Furniture image",
+  },
+  {
+    id: 48,
+    src: img_48,
+    url: "@/assets/images/furniture/image-48.jpeg",
+    alt: "Furniture image",
+  },
+];
