afde2acb38c9ba1c7111c2828dc286884abff1c4
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-04-10 01:58:25 +0200

    fix contact page

---
+++ src/components/contact-info/ContactInfo.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styles from "./ContactInfo.module.scss";
+import { useTranslation } from "next-i18next";
+import { joinClasses } from "@/common/utils/joinClasses";
+import { PhoneLogo } from "@/assets/icons/PhoneLogo";
+import { MailLogo } from "@/assets/icons/MailLogo";
+
+export const ContactInfo = () => {
+  const [t] = useTranslation();
+  const translate = (key: string) => {
+    return t(`contact.${key}`)
+      .split("\n")
+      .map((line, key) => <p key={key}>{line}</p>);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>{translate("firstSection.title")}</div>
+      <div className={styles.wrapper}>
+        <div className={styles.header}>{translate("secondSection.header")}</div>
+        <div className={styles.text}>{translate("secondSection.text")}</div>
+        <div
+          className={joinClasses(styles.text, styles.click)}
+          onClick={() => (window.location.href = "tel:509225754")}
+        >
+          <span className={styles.logo}>
+            <PhoneLogo />
+          </span>
+          +48 509 225 754
+        </div>
+        <div
+          className={joinClasses(styles.text, styles.click)}
+          onClick={() => (window.location.href = "tel:324315350")}
+        >
+          <span className={styles.logo}>
+            <PhoneLogo />
+          </span>
+          32 43 15 350
+        </div>
+        <div
+          className={joinClasses(styles.text, styles.click)}
+          onClick={() =>
+            (window.location.href = "mailto:tapicerstwostalmach@gmail.com")
+          }
+        >
+          <span className={styles.logo}>
+            <MailLogo />
+          </span>
+          tapicerstwostalmach@gmail.com
+        </div>
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.header}>{translate("thirdSection.header")}</div>
+        <div className={styles.text}>{translate("thirdSection.text")}</div>
+      </div>
+    </div>
+  );
+};
--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -2,13 +2,13 @@ import React, { ReactElement } from "react";
 import styles from "../styles/Contact.module.scss";
 import { SubpageHeader } from "@/components/subpage-header/SubpageHeader";
 import { useTranslation } from "next-i18next";
-import { SubpageSectionWrapper } from "@/components/subpage-section-wrapper/SubpageSectionWrapper";
 import { TextSection } from "@/components/text-section/TextSection";
 import { NextPageWithLayout } from "@/pages/_app";
 import { Layout } from "@/components/layout/Layout";
 import Head from "next/head";
 import { GetStaticProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { ContactInfo } from "@/components/contact-info/ContactInfo";
 
 const Kontakt: NextPageWithLayout = () => {
   const [t] = useTranslation();
@@ -19,7 +19,7 @@ const Kontakt: NextPageWithLayout = () => {
         <title>Kontakt - Tapicerstwo Stalmach</title>
       </Head>
       <SubpageHeader header={t("contact.title")} />
-      <SubpageSectionWrapper onRight={false}>
+      <div className={styles.leftWrapper}>
         <div className={styles.mapWrapper}>
           <iframe
             width="100%"
@@ -27,14 +27,13 @@ const Kontakt: NextPageWithLayout = () => {
             src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Pracownia%20Tapicerska%20%22Duo%22,%20Dworcowa%202,%20Czerwionka-Leszczyny,%20Poland+(Pracownia%20Tapicerska%20%22Duo%22)&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
           ></iframe>
         </div>
-      </SubpageSectionWrapper>
-      <SubpageSectionWrapper onRight={true}>
-        <TextSection
-          header={t("contact.firstSection.header")}
-          text={t("contact.firstSection.text")}
-          onRight={false}
-        />
-      </SubpageSectionWrapper>
+      </div>
+      <div className={styles.rightWrapper}>
+        <div className={styles.infoWrapper}>
+          <TextSection header={t("contact.header")} onRight={false} />
+          <ContactInfo />
+        </div>
+      </div>
     </>
   );
 };
