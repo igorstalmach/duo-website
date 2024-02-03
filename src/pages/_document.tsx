69093ba44f48396e044e4028111e7c170702183c
Igor Stalmach <igorstalmach@outlook.com>
2024-02-03 17:04:13 +0100

    Update favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,37 +3,43 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang={'pl'}>
-      <Head>
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta name="theme-color" content="#f5f5f7" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+        <Head>
+            <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/favicon/apple-touch-icon.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="144x144"
+                href="/favicon/favicon-144x144.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/favicon/site.webmanifest"/>
+            <link
+                rel="mask-icon"
+                href="/favicon/safari-pinned-tab.svg"
+                color="#5bbad5"
+            />
+            <meta name="theme-color" content="#f5f5f7"/>
+        </Head>
+        <body>
+        <Main/>
+        <NextScript/>
+        </body>
     </Html>
   );
 }
